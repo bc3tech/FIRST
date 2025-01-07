a6a8f4f095d91774305f446945af4c0db14a1c4b
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-07 11:09:59 -0800

    gRPC implemented for Teams agent
    Teams agent starts up and introduces to Orchestrator via gRPC endpoint
    and client
    
    User POSTS to orch http1 endpoint `api/chat/GetCompletion` with prompt
    controller uses gRPC to internally hit the orchestrator server which
    executes SK to hit the right registered agent.

---
+++ TBAStatReader_gRPC/Worker.cs
@@ -0,0 +1,73 @@
+﻿namespace TBAStatReader;
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Common;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using TBAStatReader_gRPC;
+
+internal class Worker(ILoggerFactory loggerFactory, Orchestrator_gRPC.Orchestrator.OrchestratorClient signalr) : IHostedService
+{
+    private readonly ILogger _log = loggerFactory.CreateLogger<Worker>();
+
+    internal static bool WaitingForResponse = false;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        Console.WriteLine("Welcome to the TBA Chat bot! What would you like to know about FIRST competitions, past or present?");
+
+        static async Task runSpinnerAsync(CancellationToken ct)
+        {
+            CircularCharArray progress = CircularCharArray.ProgressSpinner;
+            while (!ct.IsCancellationRequested)
+            {
+                Console.Write(progress.Next());
+                Console.CursorLeft = 0;
+
+                await Task.Delay(100, ct);
+            }
+        }
+
+        do
+        {
+            Console.Write("> ");
+            var question = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(question))
+            {
+                break;
+            }
+
+            var timer = Stopwatch.StartNew();
+
+            CancellationTokenSource spinnerCancelToken = new();
+            var combinedCancelToken = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, spinnerCancelToken.Token);
+            var t = Task.Run(() => runSpinnerAsync(combinedCancelToken.Token), combinedCancelToken.Token);
+
+            WaitingForResponse = true;
+            var answer = await signalr.GetAnswerAsync(new Expert_gRPC.AnswerRequest { Prompt = question }, cancellationToken: cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                continue;
+            }
+
+            WaitingForResponse = false;
+            await spinnerCancelToken.CancelAsync();
+            Console.CursorLeft = 0;
+
+            Console.WriteLine(answer.Completion);
+
+            _log.TimeToAnswerTta(timer.Elapsed);
+        } while (!cancellationToken.IsCancellationRequested);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
