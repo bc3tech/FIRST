9226d6bcf74f080d3d5fa616fe5708887fea12be
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-04-11 12:12:40 -0700

    cleanup

--- SignalRHub/UserIdProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SignalRHub;
-
-using Microsoft.AspNetCore.SignalR;
-
-internal class UserIdProvider() : IUserIdProvider
-{
-    public string? GetUserId(HubConnectionContext connection) => connection.GetHttpContext()?.Request.Query["userid"].ToString();
-}
