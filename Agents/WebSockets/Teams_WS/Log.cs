3a9bbd184b11ccc463009aad395dea2316eb42df
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-04-11 13:05:14 -0700

    strongly-typed logging

---
+++ Agents/WebSockets/Teams_WS/Log.cs
@@ -0,0 +1,16 @@
+﻿namespace TBAAPI.V3Client.Api;
+
+#pragma warning disable CS8019
+using Microsoft.Extensions.Logging;
+using System;
+#pragma warning restore CS8019
+
+static partial class Log
+{
+
+    [LoggerMessage(0, LogLevel.Debug, "Resulting document: {searchResults}")]
+    internal static partial void ResultingDocumentSearchResults(this ILogger logger, string searchResults);
+
+    [LoggerMessage(2, LogLevel.Trace, "JsonCons.JMESPath result: {jsonConsResult}")]
+    internal static partial void JsonConsJMESPathResultJsonConsResult(this ILogger logger, string jsonConsResult);
+}
